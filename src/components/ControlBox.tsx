7437e837cd07c091b996025ac3c82ecf22917202
Kaifkhan001 <kaifkhanksi7544@gmail.com>
2025-10-30 18:20:27 +0530

    Some more fixes and better user experience

--- src/components/ControlBox.tsx
+++ src/components/ControlBox.tsx
@@ -1,11 +1,13 @@
 import { motion } from 'framer-motion'
 import  { type ReactElement } from 'react'
+import { toast } from 'sonner'
 
 const ControlBox = ({ text, icon } : {text : string, icon: ReactElement}) => {
   return (
     <motion.div
     whileHover={{ scale: 1.02 }}
     transition={{ type: "spring", stiffness: 300 }}
+    onClick={() => toast.error("This feautes is in process to be implemented 😊")}
     >
     <div className='w-16 h-16 md:w-20 md:h-20 rounded-xl flex items-center flex-col text-sm md:text-xl  justify-center text-white bg-black border-[1px] px-4 hover:bg-gray-950 cursor-pointer'>
       {icon}
--- src/utils/getSixRandom.ts
+++ src/utils/getSixRandom.ts
@@ -9,7 +9,10 @@ export default function getSixRandom(arr: string[], Neededvalue: string){
     } 
     console.log("New 6 size arr :- ", newArr);
     console.log("The main value:- ", Neededvalue);
-    if(!newArr.includes(Neededvalue)) newArr[5] = Neededvalue;
+    if(!newArr.includes(Neededvalue)) {
+    const num = Math.floor(Math.random() * 6);
+    newArr[num] = Neededvalue;
+    };
     console.log("6th place:- ", newArr[5]);
     console.log("Whole new arr:- ", newArr);
     return newArr;
